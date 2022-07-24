acaac88d5fb45f354fb0f4334d32d2980094391a
Tuan <projectxmaker@gmail.com>
2022-07-24 22:39:12 +0700

    setup and decorate flags

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -13,9 +13,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries: [String] = []
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupCountries()
+        decorateFlags()
+        setupFlags()
+    }
+    
+    private func setupCountries() {
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    }
+    
+    private func setupFlags() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button2.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    private func decorateFlags() {
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
     }
 
 
