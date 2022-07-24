cfa62df182a80d8f619acb363fc6559013e6a022
Tuan <projectxmaker@gmail.com>
2022-07-24 15:32:44 +0700

    init project 2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Pham Anh Tuan on 7/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
