21cd391953415ab79cc650714669a56dea8638d7
Tuan <projectxmaker@gmail.com>
2022-07-28 17:20:13 +0700

    add share button

--- MilestoneProject1To3/MilestoneProject1To3/Presentation Layer/UIViewControllers/FlagDetailViewController.swift
+++ MilestoneProject1To3/MilestoneProject1To3/Presentation Layer/UIViewControllers/FlagDetailViewController.swift
@@ -15,6 +15,7 @@ class FlagDetailViewController: UIViewController {
         super.viewDidLoad()
 
         showSelectedFlag()
+        setupShareButton()
     }
     
     private func showSelectedFlag() {
@@ -23,5 +24,26 @@ class FlagDetailViewController: UIViewController {
         let finalPath = flagFolderPath.appendingPathComponent(selectedFlagName, isDirectory: false).path
         
         flagImage.image = UIImage(contentsOfFile: finalPath)
+    }
+    
+    // MARK: - Share Button
+    private func setupShareButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(handleShareButtonTapped))
+    }
+    
+    @objc private func handleShareButtonTapped() {
+        let countryName = selectedFlagName.split(separator: "@", maxSplits: 2, omittingEmptySubsequences: true)[0]
+        
+        guard let image = flagImage.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        var activityItems: [Any] = []
+        activityItems.append(image)
+        activityItems.append(countryName)
+        let av = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        
+        present(av, animated: true, completion: nil)
     }
 }
