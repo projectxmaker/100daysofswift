91b2aacde3ad8fde97217e6e6124eb41f5945620
Tuan <projectxmaker@gmail.com>
2022-07-31 22:38:38 +0700

    use safeAreaLayoutGuide instead of view for trailling anchor and leading anchor

--- Project6b/Project6b/Presentation Layer/UIViewControllers/ViewController.swift
+++ Project6b/Project6b/Presentation Layer/UIViewControllers/ViewController.swift
@@ -62,8 +62,8 @@ class ViewController: UIViewController {
         var previous: UILabel?
         
         for label in [label1, label2, label3, label4, label5] {
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
             label.heightAnchor.constraint(equalToConstant: 88).isActive = true
             
             if let previous = previous {
