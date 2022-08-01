c08864a0d87488c4e4e8504daa06a7eaca02d633
Tuan <projectxmaker@gmail.com>
2022-08-01 22:06:31 +0700

    almost done auto layout by IB, log error when turning to landscape orientation

---
+++ Project6 - Auto Layout In Code/Project6c - Auto Layout By IB/AutoLayoutByIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Pham Anh Tuan on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
