f3bb8b6ff61428c5bdf8c5b8d9fb4f4cd81ec5d1
Tuan <projectxmaker@gmail.com>
2022-08-07 21:50:49 +0700

    init code of project 10

---
+++ Project10/Project10/Presentation Layer/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Pham Anh Tuan on 8/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
