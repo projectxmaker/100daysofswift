3b4b128a13f4af2ccb58cc40a8fe0e479b575b1d
Tuan <projectxmaker@gmail.com>
2022-08-12 21:46:15 +0700

    setup UI

--- Project13/Project13/Presentation Layer/View Controllers/ViewController.swift
+++ Project13/Project13/Presentation Layer/View Controllers/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - Slider Functions
+    private func intensityChanged() {
+        
+    }
+    
+    // MARK: - IB Actions
+    @IBAction private func changeFilter(_ button: UIButton) {
+        
+    }
+    
+    @IBAction private func save(_ button: UIButton) {
+        
+    }
 }
 
