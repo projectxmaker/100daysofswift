48e6798663793ff408a9b4a01f2f62c86e5f4f83
Tuan <projectxmaker@gmail.com>
2022-08-13 15:15:49 +0700

    init code of project14

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Pham Anh Tuan on 8/13/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+
+}
