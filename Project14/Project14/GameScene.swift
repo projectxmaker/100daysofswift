6b0940eb7b79dfbb19f2a671ac71e5482340170f
Tuan <projectxmaker@gmail.com>
2022-08-13 21:51:07 +0700

    show/hide characters randomly with random delay

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -8,6 +8,8 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var popupTime = 0.85
+    
     var gameScore: SKLabelNode!
     var score = 0 {
         didSet {
@@ -31,20 +33,10 @@ class GameScene: SKScene {
         gameScore.horizontalAlignmentMode = .left
         addChild(gameScore)
         
-        for i in 0..<5 {
-            createSlot(at: CGPoint(x: 100 + (i * 170), y: 410))
-        }
+        setupWhackSlots()
         
-        for i in 0..<4 {
-            createSlot(at: CGPoint(x: 180 + (i * 170), y: 320))
-        }
-        
-        for i in 0..<5 {
-            createSlot(at: CGPoint(x: 100 + (i * 170), y: 230))
-        }
-        
-        for i in 0..<4 {
-            createSlot(at: CGPoint(x: 180 + (i * 170), y: 140))
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.newWave()
         }
     }
     
@@ -52,7 +44,27 @@ class GameScene: SKScene {
 
     }
     
-    private func createSlot(at: CGPoint) {
+    // MARK: - Extra Funcs
+    
+    private func setupWhackSlots() {
+        for i in 0..<5 {
+            createWhackSlot(at: CGPoint(x: 100 + (i * 170), y: 410))
+        }
+        
+        for i in 0..<4 {
+            createWhackSlot(at: CGPoint(x: 180 + (i * 170), y: 320))
+        }
+        
+        for i in 0..<5 {
+            createWhackSlot(at: CGPoint(x: 100 + (i * 170), y: 230))
+        }
+        
+        for i in 0..<4 {
+            createWhackSlot(at: CGPoint(x: 180 + (i * 170), y: 140))
+        }
+    }
+    
+    private func createWhackSlot(at: CGPoint) {
         let slot = WhackSlot()
         slot.config(at: at)
         
@@ -61,4 +73,35 @@ class GameScene: SKScene {
         slots.append(slot)
     }
 
+    private func newWave() {
+        popupTime *= 0.991
+        
+        slots.shuffle()
+        
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0..<12) > 4 {
+            slots[1].show(hideTime: popupTime)
+        }
+        
+        if Int.random(in: 0..<12) > 8 {
+            slots[2].show(hideTime: popupTime)
+        }
+        
+        if Int.random(in: 0..<12) > 10 {
+            slots[3].show(hideTime: popupTime)
+        }
+        
+        if Int.random(in: 0..<12) > 11 {
+            slots[4].show(hideTime: popupTime)
+        }
+        
+        let minDelay = popupTime * 0.5
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.newWave()
+        }
+    }
 }
--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -9,6 +9,11 @@ import UIKit
 import SpriteKit
 
 class WhackSlot: SKNode {
+    var isVisible = false
+    var isHit = false
+    
+    var penguin: SKSpriteNode!
+    
     func config(at: CGPoint) {
         position = at
         
@@ -20,11 +25,41 @@ class WhackSlot: SKNode {
         mask.position = CGPoint(x: 0, y: 10)
         mask.maskNode = SKSpriteNode(imageNamed: "whackMask")
         
-        let penguinGood = SKSpriteNode(imageNamed: "penguinGood")
-        penguinGood.position = CGPoint(x: 0, y: -90)
+        penguin = SKSpriteNode(imageNamed: "penguinGood")
+        penguin.position = CGPoint(x: 0, y: -90)
         
-        mask.addChild(penguinGood)
+        mask.addChild(penguin)
         
         addChild(mask)
+    }
+    
+    func show(hideTime: Double) {
+        guard !isVisible else { return }
+        
+        penguin.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        
+        if Int.random(in: 0..<2) == 0 {
+            penguin.texture = SKTexture(imageNamed: "penguinGood")
+            penguin.name = "penguinGood"
+        } else {
+            penguin.texture = SKTexture(imageNamed: "penguinEvil")
+            penguin.name = "penguinEvil"
+        }
+
+        isVisible = true
+        isHit = false
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime*3.5)) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        guard isVisible else { return }
+        
+        penguin.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        
+        isVisible = false
+        isHit = false
     }
 }
