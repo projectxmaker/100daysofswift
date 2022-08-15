970e074cae3e249e1ddb64f2d284ce0407e2b86f
Tuan <projectxmaker@gmail.com>
2022-08-15 08:43:40 +0700

    lay penguin and button on view programmatically

--- Project15/Project15/Presentation Layer/View Controllers/ViewController.swift
+++ Project15/Project15/Presentation Layer/View Controllers/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var switcherButton: UIButton!
+    
+    private var imageView: UIImageView!
+    private var counter = 0
+    private let totalOfAnimations = 7
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        // Do any additional setup after loading the view.
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        switcherButton = UIButton(configuration: .filled())
+        switcherButton.translatesAutoresizingMaskIntoConstraints = false
+        switcherButton.addTarget(self, action: #selector(handleSwitcherButtonTapped), for: .touchUpInside)
+        switcherButton.setTitle("Animation Switcher", for: .normal)
+        switcherButton.backgroundColor = .green
+        view.addSubview(switcherButton)
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor, constant: 0),
+            imageView.centerYAnchor.constraint(equalTo: view.layoutMarginsGuide.centerYAnchor, constant: 0),
+
+            switcherButton.heightAnchor.constraint(equalToConstant: 44),
+            switcherButton.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.3),
+            switcherButton.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor),
+            switcherButton.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: 0)
+            
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+    }
+    
+    
+    // MARK: - IB Action
+    @objc private func handleSwitcherButtonTapped() {
+        counter += 1
+        
+        if counter > totalOfAnimations {
+            counter = 0
+        }
     }
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
