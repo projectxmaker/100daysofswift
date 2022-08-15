75a44d07e6731876f3647973bc5edfc26fbd42f2
Tuan <projectxmaker@gmail.com>
2022-08-15 22:52:56 +0700

    init code of milestone project 13 - 15

---
+++ MilestoneP13To15/MilestoneP13To15/Presentation Layer/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneP13To15
+//
+//  Created by Pham Anh Tuan on 8/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
