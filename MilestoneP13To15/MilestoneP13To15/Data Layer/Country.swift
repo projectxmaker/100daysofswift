ec320466b00b95c568a3ebbc1ab5120a45ac8f4b
Tuan <projectxmaker@gmail.com>
2022-08-15 23:41:20 +0700

    implement country list screen with data is loaded from API

---
+++ MilestoneP13To15/MilestoneP13To15/Data Layer/Country.swift
@@ -0,0 +1,13 @@
+//
+//  Country.swift
+//  MilestoneP13To15
+//
+//  Created by Pham Anh Tuan on 8/15/22.
+//
+
+import Foundation
+
+class Country: Codable {
+    var name: String
+    var alpha3Code: String
+}
