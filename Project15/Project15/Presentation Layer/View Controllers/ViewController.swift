c86bfc356ae46eccd24b3541535aa92c144b5890
Tuan <projectxmaker@gmail.com>
2022-08-15 13:54:08 +0700

    allow to play animation by tapping on the button

--- Project15/Project15/Presentation Layer/View Controllers/ViewController.swift
+++ Project15/Project15/Presentation Layer/View Controllers/ViewController.swift
@@ -51,7 +51,53 @@ class ViewController: UIViewController {
     
     
     // MARK: - IB Action
-    @objc private func handleSwitcherButtonTapped() {
+    @objc private func handleSwitcherButtonTapped(_ button: UIButton) {
+        button.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 1, options: []) {
+            switch self.counter {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                break
+                
+            case 1:
+                self.imageView.transform = CGAffineTransform.identity
+                break
+                
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                break
+                
+            case 3:
+                self.imageView.transform = CGAffineTransform.identity
+                break
+                
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                break
+                
+            case 5:
+                self.imageView.transform = CGAffineTransform.identity
+                break
+                
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .green
+                break
+                
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+                break
+                
+                
+            default:
+                break
+            }
+        } completion: { finished in
+            button.isHidden = false
+        }
+
         counter += 1
         
         if counter > totalOfAnimations {
