b7d49360ec65ba7d1b7147c5895a26decf9fef57
Tuan <projectxmaker@gmail.com>
2022-08-16 23:15:14 +0700

    add Ha Noi to capital list

--- Project 16 - Map Of Capitals/MapOfCapitals/Presentation Layer/View Controllers/ViewController.swift
+++ Project 16 - Map Of Capitals/MapOfCapitals/Presentation Layer/View Controllers/ViewController.swift
@@ -23,13 +23,14 @@ class ViewController: UIViewController, MKMapViewDelegate {
 
     // MARK: - Extra Functions
     private func initCapitals() {
+        let hanoi = Capital(title: "Ha Noi", coordinate: CLLocationCoordinate2D(latitude: 21.028333, longitude: 105.854167), info: "Capital of Viet Nam", wikipediaUrl: "https://en.wikipedia.org/wiki/hanoi")
         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.", wikipediaUrl: "https://en.wikipedia.org/wiki/london")
         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.", wikipediaUrl: "https://en.wikipedia.org/wiki/oslo")
         let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.", wikipediaUrl: "https://en.wikipedia.org/wiki/paris")
         let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.", wikipediaUrl: "https://en.wikipedia.org/wiki/rome")
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.", wikipediaUrl: "https://en.wikipedia.org/wiki/Washington,_D.C.")
         
-        mapView.addAnnotations([london, oslo, paris, rome, washington])
+        mapView.addAnnotations([hanoi, london, oslo, paris, rome, washington])
     }
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
