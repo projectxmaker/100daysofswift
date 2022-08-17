e938d5b2662570a7458ba79e854f280edf77b11d
Tuan <projectxmaker@gmail.com>
2022-08-17 17:50:59 +0700

    add startfield, player and score label onto screen

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -8,10 +8,41 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
 
+    private var startField: SKEmitterNode!
+    private var player: SKSpriteNode!
+    private var scoreLabel: SKLabelNode!
+    private var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
-
+        backgroundColor = .black
+        
+        startField = SKEmitterNode(fileNamed: "starfield")
+        startField.position = CGPoint(x: 1024, y: 384)
+        startField.advanceSimulationTime(10)
+        startField.zPosition = -1
+        addChild(startField)
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        
+        guard let playerTexture = player.texture else { return }
+        player.physicsBody = SKPhysicsBody(texture: playerTexture, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
