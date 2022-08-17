570adff6475949e47c929c0750ed3de8bf3756ef
Tuan <projectxmaker@gmail.com>
2022-08-17 16:47:34 +0700

    init code of project 17

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Pham Anh Tuan on 8/17/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+}
