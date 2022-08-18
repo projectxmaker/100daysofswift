1091d18e0dbfab56c3b763fd5e6f5819d5868720
Tuan <projectxmaker@gmail.com>
2022-08-18 15:50:26 +0700

    init code for Milestone Project 16 to 18

---
+++ MilestoneP16To18/MilestoneP16To18/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  MilestoneP16To18
+//
+//  Created by Pham Anh Tuan on 8/18/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
