f09ab3bac8743c1014e024b806e9c4797fd14b69
Tuan <projectxmaker@gmail.com>
2022-08-23 06:56:21 +0700

    init code for Project 21

---
+++ Project21/Project21/Presentation Layer/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project21
+//
+//  Created by Pham Anh Tuan on 8/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
