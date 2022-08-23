cb3b1531b82b37ff709716dd2d7d6c88a475085c
Tuan <projectxmaker@gmail.com>
2022-08-23 08:00:23 +0700

    request authorization and schedule notification w/ calendar trigger

--- Project21/Project21/Presentation Layer/View Controllers/ViewController.swift
+++ Project21/Project21/Presentation Layer/View Controllers/ViewController.swift
@@ -6,12 +6,48 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(handleRegisterButtonTapped))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(handleScheduleButtonTapped))
+    }
+    
+    @objc private func handleRegisterButtonTapped() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.badge, .alert, .sound]) { granted, error in
+            if granted {
+                print("granted")
+            } else {
+                print("oops")
+            }
+        }
+    }
+    
+    @objc private func handleScheduleButtonTapped() {
+        let center = UNUserNotificationCenter.current()
+
+        let content = UNMutableNotificationContent()
+        content.title = "Title here"
+        content.subtitle = "Subtitle here"
+        content.body = "Body here"
+        content.sound = .default
+        content.badge = 10
+        content.categoryIdentifier = ""
+        content.userInfo = ["data": "something"]
+
+        var triggerDate = DateComponents()
+        triggerDate.hour = 7
+        triggerDate.minute = 49
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: true)
+
+        center.add(UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger))
+
     }
 
 
