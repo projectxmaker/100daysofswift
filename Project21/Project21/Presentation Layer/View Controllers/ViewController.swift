986a99ee22c94eb4e50358bc7a32332a047ddf16
Tuan <projectxmaker@gmail.com>
2022-08-23 10:03:47 +0700

    add show button on the notification

--- Project21/Project21/Presentation Layer/View Controllers/ViewController.swift
+++ Project21/Project21/Presentation Layer/View Controllers/ViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    let triggerNotificationAfterTimeInterval = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
             UIBarButtonItem(title: "Schedule w/ Interval", style: .plain, target: self, action: #selector(handleScheduleWithTimeIntervalButtonTapped))
         ]
     }
+    
+    // MARK: - Button Handlers
     
     @objc private func handleRegisterButtonTapped() {
         let center = UNUserNotificationCenter.current()
@@ -32,22 +35,6 @@ class ViewController: UIViewController {
         }
     }
     
-    private func scheduleNotification(trigger: UNNotificationTrigger) {
-        let center = UNUserNotificationCenter.current()
-
-        let content = UNMutableNotificationContent()
-        content.title = "Title here"
-        content.subtitle = "Subtitle here"
-        content.body = "Body here"
-        content.sound = .default
-        content.badge = 10
-        content.categoryIdentifier = ""
-        content.userInfo = ["data": "something"]
-
-        center.add(UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger))
-
-    }
-    
     @objc private func handleScheduleWithCalendarButtonTapped() {
         var triggerDate = DateComponents()
         triggerDate.hour = 8
@@ -58,11 +45,57 @@ class ViewController: UIViewController {
     }
     
     @objc private func handleScheduleWithTimeIntervalButtonTapped() {
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(triggerNotificationAfterTimeInterval), repeats: false)
         
         scheduleNotification(trigger: trigger)
     }
+    
+    // MARK: - Notification Center Delegate
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let data = userInfo["data"] as? String {
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                // do something
+                print("swipe ... \(data)")
+            case "show":
+                // do something
+                print("tap show ... \(data)")
+            default:
+                break
+            }
+        }
 
+    }
+    
+    // MARK: - Extra Funcs
+    
+    private func scheduleNotification(trigger: UNNotificationTrigger) {
+        registerCategory()
+        
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Take my son from school"
+        content.subtitle = "don't forget icecream & pancake"
+        content.body = "4:30PM at Hoa Hong school"
+        content.sound = .default
+        content.badge = 10
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["data": "something"]
 
+        center.add(UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger))
+
+    }
+
+    private func registerCategory() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Show", options: .foreground, icon: UNNotificationActionIcon(systemImageName: "rectangle.portrait.and.arrow.right.fill"))
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        center.setNotificationCategories([category])
+    }
 }
 
