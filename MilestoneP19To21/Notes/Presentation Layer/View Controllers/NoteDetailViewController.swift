6c37068a4f1ef8b4459e20cf1f35e41ac10d5720
Tuan <projectxmaker@gmail.com>
2022-08-27 16:15:05 +0700

    make current selection is visible by auto scrolling

--- MilestoneP19To21/Notes/Presentation Layer/View Controllers/NoteDetailViewController.swift
+++ MilestoneP19To21/Notes/Presentation Layer/View Controllers/NoteDetailViewController.swift
@@ -23,6 +23,8 @@ class NoteDetailViewController: UIViewController {
         setupNavigationItems()
         
         setupToolbars()
+        
+        addObserverForKeyboardNotification()
     }
     
 
@@ -92,5 +94,32 @@ class NoteDetailViewController: UIViewController {
     
     private func getNoteByIndex(_ index: Int) -> Note? {
         return nil
+    }
+    
+    // MARK: - Keyboards
+    private func addObserverForKeyboardNotification() {
+        let notification = NotificationCenter.default
+        
+        notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = .zero
+        } else {
+            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        textView.scrollIndicatorInsets = textView.contentInset
+
+        let selectedRange = textView.selectedRange
+        textView.scrollRangeToVisible(selectedRange)
     }
 }
