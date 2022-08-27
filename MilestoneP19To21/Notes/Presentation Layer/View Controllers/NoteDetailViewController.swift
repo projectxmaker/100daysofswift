e4a9371e96843df6bc99f8848e1b4cc87a098d2f
Tuan <projectxmaker@gmail.com>
2022-08-27 22:39:21 +0700

    hide/unhide Done button when editing the text

--- MilestoneP19To21/Notes/Presentation Layer/View Controllers/NoteDetailViewController.swift
+++ MilestoneP19To21/Notes/Presentation Layer/View Controllers/NoteDetailViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 
-class NoteDetailViewController: UIViewController {
+class NoteDetailViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var textView: UITextView!
     
     var noteIndex: Int?
     var doneButton: UIBarButtonItem!
+    var shareButton: UIBarButtonItem!
+    
+    private var isDoneButtonHidden = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,8 @@ class NoteDetailViewController: UIViewController {
         setupToolbars()
         
         addObserverForKeyboardNotification()
+        
+        textView.delegate = self
     }
     
 
@@ -38,17 +43,68 @@ class NoteDetailViewController: UIViewController {
     }
     */
     
+    // MARK: - Keyboards
+    private func addObserverForKeyboardNotification() {
+        let notification = NotificationCenter.default
+        
+        notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        hideDoneButton(false)
+        makeTextViewSelectionVisible(notification: notification)
+    }
+    
+    // MARK: - Text View
+    func textViewDidChange(_ textView: UITextView) {
+        hideDoneButton(false)
+    }
+    
+    private func makeTextViewSelectionVisible(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = .zero
+        } else {
+            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        textView.scrollIndicatorInsets = textView.contentInset
+
+        let selectedRange = textView.selectedRange
+        textView.scrollRangeToVisible(selectedRange)
+    }
+    
     // MARK: - Extra Funcs
+    private func hideDoneButton(_ isHide: Bool = true) {
+        if isHide {
+            if !isDoneButtonHidden {
+                // hide doneButton
+                navigationItem.rightBarButtonItems = [shareButton]
+                isDoneButtonHidden = true
+            }
+        } else {
+            if isDoneButtonHidden {
+                // show doneButton
+                navigationItem.rightBarButtonItems = [doneButton, shareButton]
+                isDoneButtonHidden = false
+            }
+        }
+    }
+    
     private func setupNavigationItems() {
         
-        let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(handleShareButtonTapped))
+        shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(handleShareButtonTapped))
         
         doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(handleDoneButtonTapped))
         
-        //navigationItem.rightBarButtonItems = [shareButton, doneButton]
-        
-        // hide doneButton as default
-        navigationItem.rightBarButtonItems = [shareButton]
+        // hide doneButton
+        hideDoneButton()
     }
     
     private func setupToolbars() {
@@ -70,6 +126,8 @@ class NoteDetailViewController: UIViewController {
     }
     
     @objc private func handleDoneButtonTapped() {
+        hideDoneButton()
+        
         // save the current note
     }
     
@@ -94,32 +152,5 @@ class NoteDetailViewController: UIViewController {
     
     private func getNoteByIndex(_ index: Int) -> Note? {
         return nil
-    }
-    
-    // MARK: - Keyboards
-    private func addObserverForKeyboardNotification() {
-        let notification = NotificationCenter.default
-        
-        notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        notification.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-    }
-    
-    @objc func adjustForKeyboard(notification: Notification) {
-        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-
-        let keyboardScreenEndFrame = keyboardValue.cgRectValue
-        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
-
-        if notification.name == UIResponder.keyboardWillHideNotification {
-            textView.contentInset = .zero
-        } else {
-            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
-        }
-
-        textView.scrollIndicatorInsets = textView.contentInset
-
-        let selectedRange = textView.selectedRange
-        textView.scrollRangeToVisible(selectedRange)
     }
 }
