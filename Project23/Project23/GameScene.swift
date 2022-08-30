482ab6702be7d3af72b813dfe92870738f3fe744
Tuan <projectxmaker@gmail.com>
2022-08-30 18:00:09 +0700

    Init code of project 23

---
+++ Project23/Project23/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by Pham Anh Tuan on 8/30/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+}
