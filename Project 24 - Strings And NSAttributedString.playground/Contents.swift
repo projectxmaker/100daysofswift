703727538489b6b5f7a4e1438a36379d7e27ec34
Tuan <projectxmaker@gmail.com>
2022-09-01 22:07:13 +0700

    challenge 1 of project 24: extend String with method withPrefix

--- Project 24 - Strings And NSAttributedString.playground/Contents.swift
+++ Project 24 - Strings And NSAttributedString.playground/Contents.swift
@@ -3,18 +3,24 @@
 import UIKit
 import PlaygroundSupport
 
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        var finalString = self
+        if !self.hasPrefix(prefix) {
+            finalString = prefix + self
+        }
+        
+        return finalString
+    }
+}
+
 class MyViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
 
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
-        self.view = view
+        let carPet = "pet".withPrefix("car")
+        print(carPet)
     }
 }
 // Present the view controller in the Live View window
