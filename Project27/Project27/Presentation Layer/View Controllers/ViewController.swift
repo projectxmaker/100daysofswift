1d53d42ca9992379930a7c8932fa7b0b4783c466
Tuan <projectxmaker@gmail.com>
2022-09-07 09:43:30 +0700

    Init code for project 27

---
+++ Project27/Project27/Presentation Layer/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Pham Anh Tuan on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
