c37578ab1cb98c3d4e9751d2637d20e3e15c0a36
Tuan <projectxmaker@gmail.com>
2022-09-07 16:24:35 +0700

    draw rotated squares by using translateBy and rotate

--- Project27/Project27/Presentation Layer/View Controllers/ViewController.swift
+++ Project27/Project27/Presentation Layer/View Controllers/ViewController.swift
@@ -49,6 +49,8 @@ class SanboxViewController: UIViewController {
             drawCircle()
         case 2:
             drawCheckerboard()
+        case 3:
+            drawRotatedSquares()
         default:
             break
         }
@@ -103,6 +105,27 @@ class SanboxViewController: UIViewController {
                     }
                 }
             }
+        }
+
+        imageView.image = img
+    }
+    
+    func drawRotatedSquares() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+        let img = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+
+            let rotations = 16
+            let amount = Double.pi / Double(rotations)
+
+            for _ in 0 ..< rotations {
+                ctx.cgContext.rotate(by: CGFloat(amount))
+                ctx.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+            }
+
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
         }
 
         imageView.image = img
