5ed45864717e2d61b9c057750328ac7c76b23f4e
Tuan <projectxmaker@gmail.com>
2022-09-07 10:05:04 +0700

    add image view, redraw button on screen and their init code

--- Project27/Project27/Presentation Layer/View Controllers/ViewController.swift
+++ Project27/Project27/Presentation Layer/View Controllers/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SanboxViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var currentDrawType = 0
+
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        drawRectangle()
     }
+    
 
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - IBActions
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+
+        default:
+            break
+        }
+    }
+    
+    // MARK: - Extra Funcs
+    func drawRectangle() {
+
+    }
+    
 }
-
