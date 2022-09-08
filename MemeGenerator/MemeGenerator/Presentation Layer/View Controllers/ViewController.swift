d260fb1afe7b352e74d3101d07fc0969ff5947bc
Tuan <projectxmaker@gmail.com>
2022-09-08 15:47:36 +0700

    Init code of Milstone Project 25-27

---
+++ MemeGenerator/MemeGenerator/Presentation Layer/View Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MemeGenerator
+//
+//  Created by Pham Anh Tuan on 9/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
