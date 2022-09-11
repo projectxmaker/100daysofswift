74ec842f95b298ed20246533f3714dfc116ca1e2
Tuan <projectxmaker@gmail.com>
2022-09-11 22:50:54 +0700

    authenticate by face id or touch id

--- SecretSwift/SecretSwift/Presentation Layer/View Controllers/ViewController.swift
+++ SecretSwift/SecretSwift/Presentation Layer/View Controllers/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Pham Anh Tuan on 9/11/22.
 //
 
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
@@ -65,8 +66,35 @@ class ViewController: UIViewController {
         title = "Nothing to see here"
     }
     
+    func authenticateWithBiometric() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [weak self] success, authenticationError in
+
+                DispatchQueue.main.async {
+                    if success {
+                        self?.unlockSecretMessage()
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
     @IBAction func authenticateTapped(_ sender: Any) {
-        unlockSecretMessage()
+        authenticateWithBiometric()
     }
 }
 
