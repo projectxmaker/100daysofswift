bac9dcfadb2b1f2a349d64df02b161da5e5d96a6
Tuan <projectxmaker@gmail.com>
2022-09-12 00:56:05 +0700

    challeng 2 of project 28: allow to use password to unlock when Face ID / Touch ID failed

--- SecretSwift/SecretSwift/Presentation Layer/View Controllers/ViewController.swift
+++ SecretSwift/SecretSwift/Presentation Layer/View Controllers/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var secret: UITextView!
     var doneButton: UIBarButtonItem!
+    var enablePasswordButton: UIBarButtonItem!
+    var setPasswordButton: UIBarButtonItem!
     
     var hideDoneButton = true {
         willSet {
@@ -22,6 +24,21 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    var hidePasswordButton = true {
+        willSet {
+            setupVisibilityOfPasswordButtons(hidePasswordButton: newValue, enablePassword: enablePassword)
+        }
+    }
+    
+    var enablePassword = false {
+        willSet {
+            setupVisibilityOfPasswordButtons(hidePasswordButton: hidePasswordButton, enablePassword: newValue)
+        }
+    }
+    
+    let keySecretPassword = "SecrectPassword"
+    let keySecretMessage = "SecretMessage"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,8 +55,10 @@ class ViewController: UIViewController {
         title = "Nothing to see here"
         
         doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(saveSecretMessage))
+        
+        setupPasswordButtons()
     }
-
+    
     // MARK: - Keyboard Change
     @objc func adjustForKeyboard(notification: Notification) {
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
@@ -64,22 +83,29 @@ class ViewController: UIViewController {
         secret.isHidden = false
         title = "Secret stuff!"
 
-        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
+        if let text = KeychainWrapper.standard.string(forKey: keySecretMessage) {
             secret.text = text
         }
         
         hideDoneButton = false
+        hidePasswordButton = false
     }
     
     @objc func saveSecretMessage() {
         guard secret.isHidden == false else { return }
 
-        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        KeychainWrapper.standard.set(secret.text, forKey: keySecretMessage)
         secret.resignFirstResponder()
         secret.isHidden = true
         title = "Nothing to see here"
         
         hideDoneButton = true
+        hidePasswordButton = true
+    }
+    
+    // MARK: - IBAction Functions
+    @IBAction func authenticateTapped(_ sender: Any) {
+        authenticateWithBiometric()
     }
     
     func authenticateWithBiometric() {
@@ -97,20 +123,125 @@ class ViewController: UIViewController {
                         self?.unlockSecretMessage()
                     } else {
                         let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
-                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        ac.addTextField { textfield in
+                            textfield.placeholder = "Input password"
+                        }
+                        ac.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak ac, weak self] _ in
+                            let password = ac?.textFields?[0].text ?? ""
+                            self?.evaluateInputtedPassword(password)
+                        }))
+                        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
                         self?.present(ac, animated: true)
                     }
                 }
             }
         } else {
             let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            ac.addTextField { textfield in
+                textfield.placeholder = "Input password"
+            }
+            ac.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak ac, weak self] _ in
+                let password = ac?.textFields?[0].text ?? ""
+                self?.evaluateInputtedPassword(password)
+            }))
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
             present(ac, animated: true)
         }
     }
     
-    @IBAction func authenticateTapped(_ sender: Any) {
-        authenticateWithBiometric()
+    // MARK: - Password Functions
+    func setupPasswordButtons() {
+        enablePasswordButton = UIBarButtonItem(image: UIImage(systemName: "lock"), style: .plain, target: self, action: #selector(enablePasswordFeature))
+        
+        setPasswordButton = UIBarButtonItem(image: UIImage(systemName: "key"), style: .plain, target: self, action: #selector(setPasswordButtonTapped))
+        
+        if let _ = getPassword() {
+            enablePassword = true
+        }
+    }
+    
+    func setupVisibilityOfPasswordButtons(hidePasswordButton: Bool, enablePassword: Bool) {
+        if hidePasswordButton == false {
+            if enablePassword == true {
+                enablePasswordButton.image = UIImage(systemName: "lock.slash")
+                navigationItem.leftBarButtonItems = [enablePasswordButton, setPasswordButton]
+            } else {
+                enablePasswordButton.image = UIImage(systemName: "lock")
+                navigationItem.leftBarButtonItems = [enablePasswordButton]
+            }
+        } else {
+            navigationItem.leftBarButtonItems = nil
+        }
+    }
+    
+    @objc func setPasswordButtonTapped() {
+        showAlertToSetPassword()
+    }
+    
+    func showAlertToSetPassword(_ invalidPasswordErrorMsg: String? = "") {
+        let ac = UIAlertController(title: "Set Password", message: invalidPasswordErrorMsg ?? nil, preferredStyle: .alert)
+        ac.addTextField { textfield in
+            textfield.placeholder = "New password"
+            textfield.isSecureTextEntry = true
+        }
+        ac.addTextField { textfield in
+            textfield.placeholder = "Retype new password"
+            textfield.isSecureTextEntry = true
+        }
+        ac.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak ac, weak self] _ in
+            let password = ac?.textFields?[0].text ?? ""
+            let retypedPassword = ac?.textFields?[1].text ?? ""
+            
+            if password != "" && password == retypedPassword {
+                self?.setPassword(password)
+            } else {
+                self?.showAlertToSetPassword("Password is invalid")
+            }
+        }))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func setPassword(_ password: String) {
+        KeychainWrapper.standard.set(password, forKey: keySecretPassword)
+    }
+    
+    func evaluateInputtedPassword(_ password: String) {
+        if let storedPassword = KeychainWrapper.standard.string(forKey: "SecrectPassword") {
+            if storedPassword == password {
+                unlockSecretMessage()
+            } else {
+                showAlertOfInvalidPassword()
+            }
+        } else {
+            showAlertOfInvalidPassword()
+        }
+    }
+    
+    func showAlertOfInvalidPassword() {
+        let ac = UIAlertController(title: "Invalid Password", message: "Inputted password is incorrect.", preferredStyle: .alert)
+        ac.addTextField { textfield in
+            textfield.placeholder = "Re-input password"
+            textfield.isSecureTextEntry = true
+        }
+        ac.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak ac, weak self] _ in
+            let password = ac?.textFields?[0].text ?? ""
+            self?.evaluateInputtedPassword(password)
+        }))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func getPassword() -> String? {
+        KeychainWrapper.standard.string(forKey: keySecretPassword)
+    }
+    
+    @objc func enablePasswordFeature() {
+        if enablePassword == false {
+            enablePassword = true
+        } else {
+            enablePassword = false
+        }
     }
 }
 
