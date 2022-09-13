8778bbcfc7020e8f14591dfd5f07fddb0c173fcd
Tuan <projectxmaker@gmail.com>
2022-09-13 20:55:32 +0700

    create BuildingNode

--- Project29/Project29/GameScene.swift
+++ Project29/Project29/GameScene.swift
@@ -8,6 +8,12 @@
 import SpriteKit
 import GameplayKit
 
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
+
 class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
