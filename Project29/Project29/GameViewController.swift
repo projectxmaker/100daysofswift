6070612bbacbc7ea71ac835e1887c5c44264440a
Tuan <projectxmaker@gmail.com>
2022-09-13 22:38:43 +0700

    create sliders, button to handle anglangle, velocity, player turn and launch button

--- Project29/Project29/GameViewController.swift
+++ Project29/Project29/GameViewController.swift
@@ -11,8 +11,20 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    var currentGame: GameScene!
+    
+    @IBOutlet weak var angleSlider: UISlider!
+    @IBOutlet weak var angleLabel: UILabel!
+    @IBOutlet weak var velocitySlider: UISlider!
+    @IBOutlet weak var velocityLabel: UILabel!
+    @IBOutlet weak var launchButton: UIButton!
+    @IBOutlet weak var playerNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        angleChanged(angleSlider)
+        velocityChanged(velocitySlider)
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
@@ -22,6 +34,9 @@ class GameViewController: UIViewController {
                 
                 // Present the scene
                 view.presentScene(scene)
+                
+                currentGame = scene as? GameScene
+                currentGame.viewController = self
             }
             
             view.ignoresSiblingOrder = true
@@ -41,5 +56,43 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    // MARK: - Extra Functions
+    func activatePlayer(number: Int) {
+        if number == 1 {
+            playerNumber.text = "<<< PLAYER ONE"
+        } else {
+            playerNumber.text = "PLAYER TWO >>>"
+        }
+
+        angleSlider.isHidden = false
+        angleLabel.isHidden = false
+
+        velocitySlider.isHidden = false
+        velocityLabel.isHidden = false
+
+        launchButton.isHidden = false
+    }
+    
+    // MARK: - IBAction Functions
+    @IBAction func angleChanged(_ sender: UISlider) {
+        angleLabel.text = "Angle: \(Int(angleSlider.value))°"
+    }
+    
+    @IBAction func velocityChanged(_ sender: UISlider) {
+        velocityLabel.text = "Velocity: \(Int(velocitySlider.value))°"
+    }
+    
+    @IBAction func launch(_ sender: Any) {
+        angleSlider.isHidden = true
+        angleLabel.isHidden = true
+
+        velocitySlider.isHidden = true
+        velocityLabel.isHidden = true
+
+        launchButton.isHidden = true
+
+        currentGame.launch(angle: Int(angleSlider.value), velocity: Int(velocitySlider.value))
     }
 }
