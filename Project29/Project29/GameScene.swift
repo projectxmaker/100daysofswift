87766080a2cfb32ddafa6df63da0b03810694b42
Tuan <projectxmaker@gmail.com>
2022-09-13 15:46:36 +0700

    Init code for Project 29

---
+++ Project29/Project29/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Pham Anh Tuan on 9/13/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+    }
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
