048a9f4e96039dfeac7b7d3c8012f67834ba89db
Tuan <projectxmaker@gmail.com>
2022-09-17 13:03:09 +0700

    Init code for Milestone Project 28 To 30

---
+++ MilestoneP28To30/MilestoneP28To30/Presentation Layer/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneP28To30
+//
+//  Created by Pham Anh Tuan on 9/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
