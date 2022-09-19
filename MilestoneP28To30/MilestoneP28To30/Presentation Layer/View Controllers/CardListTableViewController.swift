58af920c16ec4ba3b9d43246e3eb2580ca209b0c
Tuan <projectxmaker@gmail.com>
2022-09-19 14:45:43 +0700

    Card List screen: read/write card pairs from/to JSON-based file

--- MilestoneP28To30/MilestoneP28To30/Presentation Layer/View Controllers/CardListTableViewController.swift
+++ MilestoneP28To30/MilestoneP28To30/Presentation Layer/View Controllers/CardListTableViewController.swift
@@ -160,20 +160,25 @@ class CardListTableViewController: UITableViewController {
     
     // MARK: - Extra Funcs
     @objc func loadCards() {
+        var cardFileUrl: URL
+        
+        // if user-defined cards file txt exists, load it, otherwise, use app-defined cards file
+        if hasUserDefinedCardFile() {
+            cardFileUrl = getUserDefinedCarfFileURL()
+        } else {
+            guard
+                let tmpURL = Bundle.main.url(forResource: "cards", withExtension: "txt")
+            else { return }
+            cardFileUrl = tmpURL
+        }
+        
         guard
-            let cardFileUrl = Bundle.main.url(forResource: "cards", withExtension: "txt"),
-            let fileContent = try? String(contentsOfFile: cardFileUrl.path)
+            let data = try? Data(contentsOf: cardFileUrl),
+            let decodedData = try? JSONDecoder().decode([Card].self, from: data)
         else { return }
         
-        let lines = fileContent.components(separatedBy: "\n")
-        
-        for eachLine in lines {
-            if eachLine.isEmpty {
-               continue
-            }
-            
-            let cardInfo = eachLine.components(separatedBy: "|")
-            let card = Card(first: cardInfo[0], second: cardInfo[1])
+        for card in decodedData {
+            let card = Card(first: card.first, second: card.second)
             cards.append(card)
         }
         
@@ -186,6 +191,9 @@ class CardListTableViewController: UITableViewController {
         let card = Card(first: first, second: second)
         cards.insert(card, at: 0)
         tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+        
+        // update user-defined cards file
+        updateUserDefinedCardsFile()
     }
     
     func showEditAlertForCard(at: IndexPath) {
@@ -228,6 +236,9 @@ class CardListTableViewController: UITableViewController {
     func editCard(at: IndexPath, first: String, second: String) {
         cards[at.row] = Card(first: first, second: second)
         tableView.reloadRows(at: [at], with: .automatic)
+        
+        // update user-defined cards file
+        updateUserDefinedCardsFile()
     }
     
     func showAlertToConfirmDeletion(at: IndexPath) {
@@ -257,5 +268,54 @@ class CardListTableViewController: UITableViewController {
     func deleteCard(at: IndexPath) {
         cards.remove(at: at.row)
         tableView.deleteRows(at: [at], with: .automatic)
+        
+        // update user-defined cards file
+        updateUserDefinedCardsFile()
+    }
+    
+    func getURLOfAppDocumentDirectory() -> URL {
+        let fm = FileManager.default
+        let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        return urls[0]
+    }
+
+    func getUserDefinedCarfFileURL() -> URL {
+        let userDefinedFileName = "user-defined-cards.txt"
+        let appDocumentDirectionUrl = getURLOfAppDocumentDirectory()
+        var userDefinedFileURL: URL
+        
+        if #available(iOS 16.0, *) {
+            userDefinedFileURL = appDocumentDirectionUrl.appending(component: userDefinedFileName)
+        } else {
+            userDefinedFileURL = appDocumentDirectionUrl.appendingPathComponent(userDefinedFileName)
+        }
+        
+        return userDefinedFileURL
+    }
+    
+    func getPathOfURL(_ url: URL) -> String {
+        var userDefinedFilePath: String
+        
+        if #available(iOS 16.0, *) {
+            userDefinedFilePath = url.path()
+        } else {
+            userDefinedFilePath = url.path
+        }
+        
+        return userDefinedFilePath
+    }
+    
+    func hasUserDefinedCardFile() -> Bool {
+        let userDefinedFileURL = getUserDefinedCarfFileURL()
+        return FileManager.default.fileExists(atPath: getPathOfURL(userDefinedFileURL))
+    }
+    
+    func updateUserDefinedCardsFile() {
+        DispatchQueue.global().async {
+            let fileURL = self.getUserDefinedCarfFileURL()
+            
+            let data = try? JSONEncoder().encode(self.cards)
+            try? data?.write(to: fileURL)
+        }
     }
 }
