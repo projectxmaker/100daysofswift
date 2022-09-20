c0c3b9fb432c14f70a28779b60191fc10cb8098c
Tuan <projectxmaker@gmail.com>
2022-09-20 22:55:29 +0700

    Settings screen: refactor code

--- MilestoneP28To30/MilestoneP28To30/Presentation Layer/View Controllers/SettingsViewController.swift
+++ MilestoneP28To30/MilestoneP28To30/Presentation Layer/View Controllers/SettingsViewController.swift
@@ -8,88 +8,29 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    var settingsTitleLabel: UILabel!
+    var biometricButton: UIButton!
+    var passcodeButton: UIButton!
+    var setPasscodeButton: UIButton!
+    var resetCardsButton: UIButton!
+    
+    var layoutConstraints = [NSLayoutConstraint]()
+    
+    var tinted = UIButton.Configuration.tinted()
+    
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
-        
-        let settingsTitleLabel = UILabel()
-        settingsTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        settingsTitleLabel.text = "Settings"
-        settingsTitleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
-        settingsTitleLabel.textAlignment = .center
-        view.addSubview(settingsTitleLabel)
 
-        var tinted = UIButton.Configuration.tinted()
         tinted.buttonSize = .large
         tinted.titleAlignment = .center
         
-        let biometricButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 20),
-        ]
-        let biometricButtonTitleAttributedString = NSAttributedString(string: "Face ID/Touch ID: ON", attributes: biometricButtonTitleAttributedKeys)
+        setupTitleLabel()
+        setupBiometricButton()
+        setupPasscode()
+        setupResetCardButton()
 
-        let biometricButton = UIButton(configuration: tinted)
-        biometricButton.translatesAutoresizingMaskIntoConstraints = false
-        biometricButton.setAttributedTitle(biometricButtonTitleAttributedString, for: .normal)
-        view.addSubview(biometricButton)
-
-        let passcodeButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 20),
-        ]
-        let passcodeButtonTitleAttributedString = NSAttributedString(string: "Passcode: ON", attributes: passcodeButtonTitleAttributedKeys)
-
-        let passcodeButton = UIButton(configuration: tinted)
-        passcodeButton.translatesAutoresizingMaskIntoConstraints = false
-        passcodeButton.setAttributedTitle(passcodeButtonTitleAttributedString, for: .normal)
-        view.addSubview(passcodeButton)
-        
-        let setPasscodeButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 20),
-        ]
-        let setPasscodeButtonTitleAttributedString = NSAttributedString(string: "Set Passcode", attributes: setPasscodeButtonTitleAttributedKeys)
-
-        let setPasscodeButton = UIButton(configuration: tinted)
-        setPasscodeButton.translatesAutoresizingMaskIntoConstraints = false
-        setPasscodeButton.setAttributedTitle(setPasscodeButtonTitleAttributedString, for: .normal)
-        view.addSubview(setPasscodeButton)
-        
-        let resetCardsButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 20),
-            .foregroundColor: UIColor.red
-        ]
-        let resetCardsButtonTitleAttributedString = NSAttributedString(string: "Reset Cards", attributes: resetCardsButtonTitleAttributedKeys)
-
-        let resetCardsButton = UIButton(configuration: tinted)
-        resetCardsButton.translatesAutoresizingMaskIntoConstraints = false
-        resetCardsButton.setAttributedTitle(resetCardsButtonTitleAttributedString, for: .normal)
-        view.addSubview(resetCardsButton)
-        
-        resetCardsButton.addTarget(self, action: #selector(handleResetCardsButtonTapped), for: .touchUpInside)
-        
-
-        NSLayoutConstraint.activate([
-            settingsTitleLabel.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
-            settingsTitleLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5),
-            settingsTitleLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            
-            biometricButton.topAnchor.constraint(equalTo: settingsTitleLabel.bottomAnchor, constant: 100),
-            biometricButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3),
-            biometricButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            
-            passcodeButton.topAnchor.constraint(equalTo: biometricButton.bottomAnchor, constant: 50),
-            passcodeButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3),
-            passcodeButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            
-            setPasscodeButton.topAnchor.constraint(equalTo: passcodeButton.bottomAnchor, constant: 20),
-            setPasscodeButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3),
-            setPasscodeButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            
-            resetCardsButton.topAnchor.constraint(equalTo: setPasscodeButton.bottomAnchor, constant: 50),
-            resetCardsButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3),
-            resetCardsButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-        ])
-        
+        NSLayoutConstraint.activate(layoutConstraints)
     }
     
     override func viewDidLoad() {
@@ -108,6 +49,93 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Layout
+    func setupTitleLabel() {
+        settingsTitleLabel = UILabel()
+        settingsTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        settingsTitleLabel.text = "Settings"
+        settingsTitleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        settingsTitleLabel.textAlignment = .center
+        view.addSubview(settingsTitleLabel)
+        
+        layoutConstraints += [
+            settingsTitleLabel.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+            settingsTitleLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5),
+            settingsTitleLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ]
+    }
+    
+    func setupBiometricButton() {
+        let biometricButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 20),
+        ]
+        let biometricButtonTitleAttributedString = NSAttributedString(string: "Face ID/Touch ID: ON", attributes: biometricButtonTitleAttributedKeys)
+
+        biometricButton = UIButton(configuration: tinted)
+        biometricButton.translatesAutoresizingMaskIntoConstraints = false
+        biometricButton.setAttributedTitle(biometricButtonTitleAttributedString, for: .normal)
+        view.addSubview(biometricButton)
+        
+        layoutConstraints += [
+            biometricButton.topAnchor.constraint(equalTo: settingsTitleLabel.bottomAnchor, constant: 100),
+            biometricButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3),
+            biometricButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ]
+    }
+    
+    func setupPasscode() {
+        let passcodeButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 20),
+        ]
+        let passcodeButtonTitleAttributedString = NSAttributedString(string: "Passcode: ON", attributes: passcodeButtonTitleAttributedKeys)
+
+        passcodeButton = UIButton(configuration: tinted)
+        passcodeButton.translatesAutoresizingMaskIntoConstraints = false
+        passcodeButton.setAttributedTitle(passcodeButtonTitleAttributedString, for: .normal)
+        view.addSubview(passcodeButton)
+        
+        let setPasscodeButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 20),
+        ]
+        let setPasscodeButtonTitleAttributedString = NSAttributedString(string: "Set Passcode", attributes: setPasscodeButtonTitleAttributedKeys)
+
+        setPasscodeButton = UIButton(configuration: tinted)
+        setPasscodeButton.translatesAutoresizingMaskIntoConstraints = false
+        setPasscodeButton.setAttributedTitle(setPasscodeButtonTitleAttributedString, for: .normal)
+        view.addSubview(setPasscodeButton)
+        
+        layoutConstraints += [
+            passcodeButton.topAnchor.constraint(equalTo: biometricButton.bottomAnchor, constant: 50),
+            passcodeButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3),
+            passcodeButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            
+            setPasscodeButton.topAnchor.constraint(equalTo: passcodeButton.bottomAnchor, constant: 20),
+            setPasscodeButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3),
+            setPasscodeButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ]
+    }
+    
+    func setupResetCardButton() {
+        let resetCardsButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 20),
+            .foregroundColor: UIColor.red
+        ]
+        let resetCardsButtonTitleAttributedString = NSAttributedString(string: "Reset Cards", attributes: resetCardsButtonTitleAttributedKeys)
+
+        let resetCardsButton = UIButton(configuration: tinted)
+        resetCardsButton.translatesAutoresizingMaskIntoConstraints = false
+        resetCardsButton.setAttributedTitle(resetCardsButtonTitleAttributedString, for: .normal)
+        view.addSubview(resetCardsButton)
+        
+        resetCardsButton.addTarget(self, action: #selector(handleResetCardsButtonTapped), for: .touchUpInside)
+        
+        layoutConstraints += [
+            resetCardsButton.topAnchor.constraint(equalTo: setPasscodeButton.bottomAnchor, constant: 50),
+            resetCardsButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3),
+            resetCardsButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ]
+    }
 
     // MARK: - Handled Methods Of Buttons
     @objc func handleResetCardsButtonTapped() {
