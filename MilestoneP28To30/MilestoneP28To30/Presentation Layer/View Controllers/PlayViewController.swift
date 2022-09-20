20927f4707c1f7af32db2948c51a542e5e820d57
Tuan <projectxmaker@gmail.com>
2022-09-20 09:14:53 +0700

    Play screen: refactor code

--- MilestoneP28To30/MilestoneP28To30/Presentation Layer/View Controllers/PlayViewController.swift
+++ MilestoneP28To30/MilestoneP28To30/Presentation Layer/View Controllers/PlayViewController.swift
@@ -10,78 +10,18 @@ import UIKit
 class PlayViewController: UIViewController {
 
     var buttonCards = [UIButton]()
+    var layoutConstraints = [NSLayoutConstraint]()
+    var cancelButton: UIButton!
     
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
-        
-        let roundLabel = UILabel()
-        roundLabel.translatesAutoresizingMaskIntoConstraints = false
-        roundLabel.text = "Round 1"
-        roundLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
-        roundLabel.textAlignment = .center
-        roundLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
-        view.addSubview(roundLabel)
-        
-        let buttonCardsView = UIView()
-        buttonCardsView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(buttonCardsView)
-        
-        let buttonCardWidth = 150
-        let buttonCardHeight = 150
-        let buttonCardSpacing = 5
-        
-        var tinted = UIButton.Configuration.tinted()
-        tinted.buttonSize = .large
-        
-        for curRow in 0..<4 {
-            for curCol in 0..<4 {
-                let buttonCard = UIButton(configuration: tinted, primaryAction: nil)
-                
-                var xPosition = curCol * buttonCardWidth
-                if curCol > 0 {
-                    xPosition += curCol * buttonCardSpacing
-                }
-                
-                buttonCard.frame = CGRect(x: xPosition, y: curRow * (buttonCardHeight + buttonCardSpacing) , width: buttonCardWidth, height: buttonCardHeight)
-                
-                buttonCard.titleLabel?.textAlignment = .center
-                buttonCard.titleLabel?.font = UIFont.systemFont(ofSize: 40)
-                buttonCard.setTitle("AC", for: .normal)
-                
-                buttonCards.append(buttonCard)
-                buttonCardsView.addSubview(buttonCard)
-            }
-        }
 
-        let cancelButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
-            .underlineStyle: NSUnderlineStyle.thick.rawValue,
-            .font: UIFont.preferredFont(forTextStyle: .callout)
-        ]
-        let cancelButtonTitleAttributedTitle = NSAttributedString(string: "Cancel", attributes: cancelButtonTitleAttributedKeys)
-        
-        let cancelButton = UIButton(type: .roundedRect)
-        cancelButton.translatesAutoresizingMaskIntoConstraints = false
-        cancelButton.setAttributedTitle(cancelButtonTitleAttributedTitle, for: .normal)
-        view.addSubview(cancelButton)
-        
-        cancelButton.addTarget(self, action: #selector(handleCancelButtonTapped), for: .touchUpInside)
-        
-        NSLayoutConstraint.activate([
-            roundLabel.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
-            roundLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5),
-            roundLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            
-            cancelButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
-            cancelButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.2),
-            cancelButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            
-            buttonCardsView.bottomAnchor.constraint(equalTo: cancelButton.topAnchor, constant: -20),
-            buttonCardsView.centerXAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.centerXAnchor),
-            buttonCardsView.widthAnchor.constraint(equalToConstant: 620),
-            buttonCardsView.heightAnchor.constraint(equalToConstant: 620)
-        ])
-        
+        setupRoundLabel()
+        setupCancelButton()
+        setupCardButtons()
+
+        NSLayoutConstraint.activate(layoutConstraints)
     }
     
     override func viewDidLoad() {
@@ -111,4 +51,93 @@ class PlayViewController: UIViewController {
     @objc func handleCancelButtonTapped() {
         navigationController?.popToRootViewController(animated: true)
     }
+    
+    @objc func handleCardButtonTapped(_ button: UIButton) {
+        print(">>> \(button.tag)")
+    }
+    
+    // MARK: - Layout
+    func setupRoundLabel() {
+        let roundLabel = UILabel()
+        roundLabel.translatesAutoresizingMaskIntoConstraints = false
+        roundLabel.text = "Round 1"
+        roundLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        roundLabel.textAlignment = .center
+        roundLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+        view.addSubview(roundLabel)
+        
+        // set up layout constraints
+        layoutConstraints += [
+            roundLabel.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+            roundLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5),
+            roundLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ]
+    }
+    
+    func setupCardButtons() {
+        let buttonCardsView = UIView()
+        buttonCardsView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonCardsView)
+        
+        let buttonCardWidth = 150
+        let buttonCardHeight = 150
+        let buttonCardSpacing = 5
+        
+        var tinted = UIButton.Configuration.tinted()
+        tinted.buttonSize = .large
+        
+        for curRow in 0..<4 {
+            for curCol in 0..<4 {
+                let cardButton = UIButton(configuration: tinted, primaryAction: nil)
+                
+                var xPosition = curCol * buttonCardWidth
+                if curCol > 0 {
+                    xPosition += curCol * buttonCardSpacing
+                }
+                
+                cardButton.frame = CGRect(x: xPosition, y: curRow * (buttonCardHeight + buttonCardSpacing) , width: buttonCardWidth, height: buttonCardHeight)
+                
+                cardButton.titleLabel?.textAlignment = .center
+                cardButton.titleLabel?.font = UIFont.systemFont(ofSize: 40)
+                cardButton.setTitle("AC", for: .normal)
+                cardButton.tag = curRow
+                
+                cardButton.addTarget(self, action: #selector(handleCardButtonTapped(_:)), for: .touchUpInside)
+                
+                buttonCards.append(cardButton)
+                buttonCardsView.addSubview(cardButton)
+            }
+        }
+        
+        // set up layout constraints
+        layoutConstraints += [
+            buttonCardsView.bottomAnchor.constraint(equalTo: cancelButton.topAnchor, constant: -20),
+            buttonCardsView.centerXAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.centerXAnchor),
+            buttonCardsView.widthAnchor.constraint(equalToConstant: 620),
+            buttonCardsView.heightAnchor.constraint(equalToConstant: 620)
+        ]
+    }
+    
+    func setupCancelButton() {
+        let cancelButtonTitleAttributedKeys: [NSAttributedString.Key: Any] = [
+            .underlineStyle: NSUnderlineStyle.thick.rawValue,
+            .font: UIFont.preferredFont(forTextStyle: .callout)
+        ]
+        let cancelButtonTitleAttributedTitle = NSAttributedString(string: "Cancel", attributes: cancelButtonTitleAttributedKeys)
+        
+        cancelButton = UIButton(type: .roundedRect)
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        cancelButton.setAttributedTitle(cancelButtonTitleAttributedTitle, for: .normal)
+        view.addSubview(cancelButton)
+        
+        cancelButton.addTarget(self, action: #selector(handleCancelButtonTapped), for: .touchUpInside)
+        
+        // set up layout constraints
+        layoutConstraints += [
+            cancelButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            cancelButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.2),
+            cancelButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ]
+    }
+    
 }
